11274f12ab042fd9209049ddf4fc5dcd52fb43b1
xbgbtx <you@example.com>
2022-02-28 13:33:50 +0000

    Open CV access camera code

--- opencv-test.py
+++ opencv-test.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
+from contextlib import contextmanager
 import cv2
+
+
+@contextmanager
+def VideoCapture(*args, **kwargs):
+    cap = cv2.VideoCapture(*args, **kwargs)
+    try:
+        yield cap
+    finally:
+        cap.release()
 
 def main():
     print("Open CV test")
+
+    with VideoCapture(0) as vid:
+        while(True):
+            ret, frame = vid.read()
+
+            cv2.imshow('frame', frame)
+
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+
 
 
 if __name__ == "__main__":
