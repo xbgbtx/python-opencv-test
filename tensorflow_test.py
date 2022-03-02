492eebb80c780aef3da840453bc7817cd9f91ef1
xbgbtx <you@example.com>
2022-03-02 21:14:33 +0000

    Load CUDA dll files

--- tensorflow_test.py
+++ tensorflow_test.py
@@ -1,3 +1,9 @@
+import os
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.6/bin") 
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.6/extras/CUPTI/lib64")
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.6/include")
+os.add_dll_directory("C:/Program Files/NVIDIA/CUDNN/v8.3/bin")
+
 import tensorflow as tf
 print(tf.__version__)
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
